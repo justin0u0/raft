863f78b8c0663aa7a0d38545bed79bc924d536b5
justin0u0 <mail@justin0u0.com>
2022-01-30 06:46:33 +0800

    feat: add follower disconnect test

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -62,4 +63,36 @@ func TestElectionAfterLeaderDisconnectLoop(t *testing.T) {
 
 		oldId, oldTerm = newId, newTerm
 	}
+}
+
+func TestFollowerDisconnect(t *testing.T) {
+	numNodes := 5
+
+	c := newCluster(t, numNodes)
+	defer c.shutdown()
+
+	time.Sleep(1 * time.Second)
+
+	leaderId, leaderTerm := c.checkSingleLeader()
+
+	peerId := randomPeerId(leaderId, numNodes)
+	c.disconnectAll(peerId)
+
+	c.disconnect(leaderId, peerId)
+
+	time.Sleep(1 * time.Second)
+
+	if nowId, nowTerm := c.checkSingleLeader(); nowId != leaderId || nowTerm != leaderTerm {
+		t.Fatal("should remains the same leader and the same term")
+	}
+}
+
+func randomPeerId(serverId uint32, numNodes int) uint32 {
+	peerId := serverId
+
+	for peerId == serverId {
+		peerId = rand.Uint32()%uint32(numNodes) + 1
+	}
+
+	return peerId
 }
